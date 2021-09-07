e7731131880c8515fc41ec2abc900596a26916aa
Konstantin Taletskiy <konstantin@taletskiy.com>
2021-09-07 15:36:10 -0700

    Clean up unused code

--- jupyterlab_wipp_plugin_creator/jupyterlab_plugin_creator/handlers.py
+++ jupyterlab_wipp_plugin_creator/jupyterlab_plugin_creator/handlers.py
@@ -6,41 +6,31 @@ from jupyter_server.utils import url_path_join
 import tornado
 
 
-class WippHandler(APIHandler):
-    @property
-    def wipp(self):
-        return self.settings["wipp"]
-
-# class textFieldHandler(WippHandler):
-#     @tornado.web.authenticated
-#     def get(self):
-#         response = self.wipp.check_api_is_live()
-#         self.finish(json.dumps(response))
-
-class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
+class CreatePlugin(APIHandler):
     @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "xyz": "This is /jupyterlab-plugin-creator/get_example endpoint!"
-        }))
+    def post(self):
+        """
+        POST request handler, creates a new plugin.
 
+        Input format:
+            {
+              'name': '',
+              'version': '',
+              'title': '',
+              'description': ''
+            }
+        """
+
+        data = json.loads(self.request.body.decode("utf-8"))
+        try:
+            with open("plugin.json", "w") as file:
+                file.write(json.dumps(data))
+        except:
+            self.write_error(500)
 
 def setup_handlers(web_app):
-
-
-
-
     handlers = [
-    ('/jupyterlab-plugin-creator/registerText', WippRegisterText),
-    # ('/wipp/ui_urls', WippUiUrls),
-    # ('/wipp/register', WippRegisterNotebook),
-    # ('/wipp/imageCollections', WippImageCollections),
-    # ('/wipp/imageCollections/search', WippImageCollectionsSearch),
-    # ('/wipp/csvCollections', WippCsvCollections),
-    # ('/wipp/csvCollections/search', WippCsvCollectionsSearch)
+        ('/jupyterlab-plugin-creator/registerText', CreatePlugin)
     ]
 
     base_url = web_app.settings["base_url"]
@@ -48,64 +38,3 @@ def setup_handlers(web_app):
 
     host_pattern = ".*$"
     web_app.add_handlers(host_pattern, handlers)
-    # base_url = web_app.settings["base_url"]
-    # route_pattern = url_path_join(base_url, "jupyterlab-plugin-creator", "get_example")
-    # handlers = [(route_pattern, RouteHandler)]
-    # web_app.add_handlers(host_pattern, handlers)
-
-
-
-
-
-class WippRegisterText(WippHandler):
-    # @tornado.web.authenticated
-    def post(self):
-        """
-        POST request handler, registers notebook in WIPP
-
-        Input format:
-            {
-              'path': '/home/jovyan/sample.ipynb',
-              'name': 'my-notebook-7',
-              'description': 'Image segmentation notebook'
-            }
-        """
-
-        #data is python dictionary sent from front end
-        try:
-            data = json.loads(self.request.body.decode("utf-8"))
-        except:
-            print('can\'t decode request')
-        #I notice I am missing response =
-
-        print("Check data:",data)
-        # path = r'\\wsl$\Ubuntu\home\kingston\pydev\plugin_creator_test'
-        # os.chdir(path)
-        print(os.getcwd())
-        with open("plugin.json", "w") as file1:
-            file1.write(json.dumps(data))
-
-## copied here as reference
-class WippRegisterNotebook(WippHandler):
-    @tornado.web.authenticated
-    def post(self):
-        """
-        POST request handler, registers notebook in WIPP
-
-        Input format:
-            {
-              'path': '/home/jovyan/sample.ipynb',
-              'name': 'my-notebook-7',
-              'description': 'Image segmentation notebook'
-            }
-        """
-
-        data = json.loads(self.request.body.decode("utf-8"))
-        if all(key in data for key in ("path","name","description")):
-            try:
-                response = self.wipp.register_notebook(data["path"], data["name"], data["description"])
-                self.finish(json.dumps(response))
-            except:
-                self.write_error(500)
-        else:
-            self.write_error(400)
