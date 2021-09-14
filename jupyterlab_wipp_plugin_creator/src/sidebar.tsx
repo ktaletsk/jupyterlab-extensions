e533ea41468f7da16c4e268a05afebe31ad5e095
Will Xiao <Will.Xiao@axleinfo.com>
2021-09-14 13:34:00 -0400

    API sending but formdata not updating

--- jupyterlab_wipp_plugin_creator/src/sidebar.tsx
+++ jupyterlab_wipp_plugin_creator/src/sidebar.tsx
@@ -4,12 +4,12 @@ import { IConsoleTracker } from '@jupyterlab/console';
 import { Widget, PanelLayout } from '@lumino/widgets';
 // import { TextWidget } from './textWidget'
 import { SchemaForm } from '@deathbeds/jupyterlab-rjsf';
-// import { JSONObject } from '@lumino/coreutils';
+// import { JSONObject, JSONValue } from '@lumino/coreutils';
 import { ToolbarButton } from '@jupyterlab/apputils';
 import { runIcon } from '@jupyterlab/ui-components';
 import { AddedFileWidget } from './addedFilesWidget'
 import { IStateDB } from '@jupyterlab/statedb'
-// import { requestAPI } from './handler';
+import { requestAPI } from './handler';
 
 
 
@@ -110,16 +110,21 @@ export class Creator_Sidebar extends Widget {
       }
     };
 
+    //needed to readjust position of getvalue
+    this._addFileWidget = new AddedFileWidget(state)
+    layout.addWidget(this._addFileWidget);
+    let filepaths= this._addFileWidget.getValue()
     const formData: any = {
       name: "My Plugin",
       version: "0.1.0",
       requirements: [''],
       inputs: [{}],
-      outputs: [{}]
+      outputs: [{}],
+      //manually added filepaths to the formData
+      filepaths: filepaths
     };
+    this._formdata = formData
 
-    this._addFileWidget = new AddedFileWidget(state)
-    layout.addWidget(this._addFileWidget);
 
     this._form = new SchemaForm(schema, { formData: formData });
     layout.addWidget(this._form);
@@ -130,30 +135,41 @@ export class Creator_Sidebar extends Widget {
     });
     layout.addWidget(refreshButton);
   }
-
+  // let filepaths = this._addFileWidget.getValue();
   submit() {
     console.log('Files: ', this._addFileWidget.getValue())
     console.log('Form: ', this._form.getValue())
-
     // TODO: create API request here
     // It should contain both the form data and the added files
-    // let request = {
-      
-    // }
-    // var fullRequest = {
-    //   method: 'POST',
-    //   body: JSON.stringify(request)
-    // };
+    let request = this._formdata;
+    console.log(request)
+    console.log(typeof request)
+  // return input;
+    var fullRequest = {
+        method: 'POST',
+        body: JSON.stringify(request)
+    };
 
-    // requestAPI<any>('createplugin', fullRequest)
-    //   .then(response => {
-    //     console.log('Handle json object sent:')
-    //     console.log(response)
-    //   })
-    //   .catch(() => console.log('There is an error making API request.'));
+
+
+    requestAPI<any>('createplugin', fullRequest)
+      .then(response => {
+        console.log('Handle json object sent:')
+        console.log(response)
+      })
+      .catch(() => console.log('There is an error making API request.'));
   }
+  // ReactDOM.render((
+  //   <Form schema={schema}
+  //         />
+  // ), document.getElementById("app"));
+  
 
   private _addFileWidget: AddedFileWidget;
   private _form: SchemaForm;
+
+  //created because need to access formdata inside submit function
+  private _formdata: JSON;
+  // private _formdata: JSONObject;
 
 }
